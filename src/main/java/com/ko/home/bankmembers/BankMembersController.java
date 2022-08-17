3d81a1151e1dc833539c850dff572112f0931f8b
KOOHYO <zpfhzpfh112@naver.com>
2022-08-17 18:11:39 +0900

    로그인

--- src/main/java/com/ko/home/bankmembers/BankMembersController.java
+++ src/main/java/com/ko/home/bankmembers/BankMembersController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/member/*")
@@ -15,6 +16,16 @@ public class BankMembersController {
 	@RequestMapping(value = "login.iu", method = RequestMethod.GET)
 	public String login()throws Exception{
 		System.out.println("로그인 접속 (GET)");
+		return "member/login";
+	}
+	
+	@RequestMapping(value = "login.iu", method = RequestMethod.POST)
+	public String login(BankMembersDTO bankMembersDTO)throws Exception{
+		System.out.println("로그인 접속 (POST)");
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("member/login");
+		mv.addObject("login", bankMembersDTO);
 		return "member/login";
 	}
 	
