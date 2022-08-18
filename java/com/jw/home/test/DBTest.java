63666eb3ee9a1c6962293768410bbaec7290b044
jiwon22lee <jiwon1799@gmail.com>
2022-08-18 14:29:35 +0900

    list

--- java/com/jw/home/test/DBTest.java
+++ java/com/jw/home/test/DBTest.java
@@ -1,4 +1,4 @@
-package com.ko.home;
+package com.jw.home.test;
 
 import static org.junit.Assert.*;
 
@@ -8,26 +8,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-//**는 그 하위폴더 안에까지 읽어라 라는 뜻!
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
-//abstract : 상속받아서 사용해야 사용가능
-public abstract class MyAbstractTest {
-	
+public class DBTest {
 	@Autowired
 	private SqlSession sqlSession;
 
 	@Test
 	public void test() {
 		
-		/*
-		 * assertNotNull(sqlSession);
-		 * 
-		 * fail("Not yet implemented");
-		 */
+		assertNotNull(sqlSession);
+		
+		fail("Not yet implemented");
 	}
-
-	
-
-}
---
+++ src/test/java/com/ko/home/bankbook/BankBookDAOTest.java
@@ -0,0 +1,46 @@
+package com.ko.home.bankbook;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ko.home.MyAbstractTest;
+import com.ko.home.BankBook.BankBookDAO;
+import com.ko.home.BankBook.BankBookDTO;
+
+public class BankBookDAOTest extends MyAbstractTest{
+	
+	@Autowired
+	private BankBookDAO bankBookDAO;
+	
+
+	@Test
+	public void setBankBookTest() throws Exception{
+
+		int result = bankBookDAO.setBankBook();
+		assertEquals(1, result);
+	}
+
+	/*
+	 * //객체 생성 후 주입시켜야 옴!
+	 * 
+	 * @Autowired private BankMembersDAO bankMembersDAO;
+	 * 
+	 * //@Test 어노테이션 꼭 줘야함!
+	 * 
+	 * @Test public void setJoinTest()throws Exception{ int result =
+	 * bankMembersDAO.setJoin(null); assertEquals(1, result); }
+	 * 
+	 * @Test public void getLoginTest()throws Exception{ BankMembersDTO
+	 * bankMembersDTO = bankMembersDAO.getLogin(null);
+	 * assertNotNull(bankMembersDTO); }
+	 * 
+	 * @Test public void getSearchByID()throws Exception{ List<BankMembersDTO> ar =
+	 * bankMembersDAO.getSearchByID("ad"); assertNotEquals(0, 0); }
+	 */
+}
