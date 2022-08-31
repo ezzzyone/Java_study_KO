1cb7d5f2d88a18d8b7be2a473c511ee0f4d6968c
jiwon22lee <jiwon1799@gmail.com>
2022-08-31 15:35:30 +0900

    회원가입 조건 충족

--- src/main/webapp/WEB-INF/views/member/join.jsp
+++ src/main/webapp/WEB-INF/views/member/join.jsp
@@ -13,7 +13,7 @@
 		<div class="row">
 			<h1>Join Page</h1>
 			<%--enctype 넘기는 타입. 파일은 잘게 쪼개서 넘어가야함.--%>
-			<form action="./join" method="POST" enctype="multipart/form-data">
+			<form id="frm" action="./join" method="POST" enctype="multipart/form-data">
 			
 			   <div class="mb-3">
     <label for="files" class="form-label">Photo</label>
@@ -25,18 +25,28 @@
 			    <label for="inputUserName" class="col-sm-2 col-form-label">ID</label>
 			    <div class="col-sm-10">
 			      <input type="text" name="userName" class="form-control" id="inputUserName" placeholder="ID 입력">
+			    <div id="d1"></div>
 			    </div>
 			  </div>
 			  <div class="row mb-3">
 			    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
 			    <div class="col-sm-10">
 			      <input type="password" name="passWord" class="form-control" id="inputPassword" placeholder="Password 입력">
+			    <div id="d2"></div>
+			    </div>
+			  </div>
+			  <div class="row mb-3">
+			    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+			    <div class="col-sm-10">
+			      <input type="password" name="passWord2" class="form-control" id="inputPassword2" placeholder="Password 입력">
+			    <div id="d3"></div>
 			    </div>
 			  </div>
 			  <div class="row mb-3">
 			    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
 			    <div class="col-sm-10">
 			      <input type="text" name="name" class="form-control" id="inputName" placeholder="이름 입력">
+			    <div id="d4"></div>
 			    </div>
 			  </div>
 			  
@@ -44,17 +54,19 @@
 			    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
 			    <div class="col-sm-10">
 			      <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email 입력">
+			    <div id="d5"></div>
 			    </div>
 			  </div>
 			  <div class="row mb-3">
 			    <label for="inputPhone" class="col-sm-2 col-form-label">Phone</label>
 			    <div class="col-sm-10">
 			      <input type="text" name="phone" class="form-control" id="inputPhone" placeholder="전화번호 입력">
+			   <div id="d6"></div>
 			    </div>
 			  </div>
 			  <div class="row justify-content-end">
 			  	<div >
-			  		<button type="submit" class="btn btn-primary mb-3">Sign in</button>
+			  		<button type="button" class="btn btn-primary mb-3" id="btn">Sign in</button>
 			  	</div>
 			  </div>
 			</form>
@@ -65,4 +77,9 @@
 	<c:import url="../template/footer.jsp"></c:import>
 
 </body>
+
+<script src="/resources/js/member.js"></script>
+<script>
+	checkjoin();
+</script>
 </html>
--- src/main/webapp/resources/js/member.js
+++ src/main/webapp/resources/js/member.js
@@ -81,3 +81,164 @@ function check(){
 
 
 }
+
+
+function checkjoin(){
+
+    const btn = document.getElementById("btn");
+    const uname = document.getElementById("inputUserName");
+    const pw = document.getElementById("inputPassword");
+    const pw2 = document.getElementById("inputPassword2");
+    const name = document.getElementById("inputName");
+    const email = document.getElementById("inputEmail");
+    const phone = document.getElementById("inputPhone");
+
+    const d1 = document.getElementById("d1");
+    const d2 = document.getElementById("d2");
+    const d3 = document.getElementById("d3");
+    const d4 = document.getElementById("d4");
+    const d5 = document.getElementById("d5");
+    const d6 = document.getElementById("d6");
+
+    const frm = document.getElementById("frm");
+
+
+    btn.addEventListener("click", function(){
+
+        let u = uname.value;
+        let p = pw.value;
+        let p2 = pw2.value;
+        let n = name.value;
+        let e = email.value;
+        let ph = phone.value;
+        
+        if(u.length>3 && p.length>7 && p2.length>7 && n.length>2  && e.length>2  && ph.length>2){
+            frm.submit();
+        }
+    });
+
+    uname.addEventListener("blur", function(){
+
+        let u = uname.value;
+    
+         if(u.length<3){
+            uname.focus();
+            d1.innerHTML='ID는 2글자 이상이어야합니다'
+        }
+
+    
+    });
+
+    uname.addEventListener("change", function(){
+
+        let u = uname.value;
+    
+         if(u.length>2){
+            d1.innerHTML=' '
+        }
+
+    
+    });
+
+    pw.addEventListener("blur", function(){
+
+        let p = pw.value;
+    
+         if(p.length<7){
+            pw.focus();
+            d2.innerHTML='pw는 6글자 이상이어야합니다'
+        }
+    
+    });
+
+    pw.addEventListener("change", function(){
+
+        let p = pw.value;
+    
+         if(p.length<7){
+            d2.innerHTML=' '
+        }
+    
+    });
+
+    name.addEventListener("blur", function(){
+
+        let n = name.value;
+    
+         if(n.length<2){
+            name.focus();
+            d4.innerHTML='이름은 1글자 이상이어야합니다'
+        }
+    
+    });
+
+    name.addEventListener("change", function(){
+
+        let n = name.value;
+    
+         if(n.length<2){
+          
+            d4.innerHTML=' '
+        }
+    
+    });
+
+    email.addEventListener("change", function(){
+        let e = email.value;
+    
+         if(e.length<2){
+          
+            d5.innerHTML=' '
+        }
+    
+    });
+    phone.addEventListener("blur", function(){
+        let ph = phone.value;
+    
+         if(ph.length<2){
+            phone.focus();
+            d6.innerHTML='번호는 1글자 이상이어야합니다'
+        }
+    
+    });
+
+    phone.addEventListener("change", function(){
+        let ph = phone.value;
+    
+         if(ph.length<2){
+       
+            d6.innerHTML=' '
+        }
+    
+    });
+
+    pw2.addEventListener("blur", function(){
+
+        let p = pw.value;
+        let p2 = pw2.value;
+    
+         if(p2.length!=p.length){
+            pw2.focus();
+            d3.innerHTML='비밀번호가 불일치합니다'
+        }
+    
+    });
+
+    pw2.addEventListener("change", function(){
+
+        let p = pw.value;
+        let p2 = pw2.value;
+    
+         if(p2.length!=p.length){
+       
+            d3.innerHTML=' '
+        }
+    
+    });
+    
+
+
+
+
+
+}
