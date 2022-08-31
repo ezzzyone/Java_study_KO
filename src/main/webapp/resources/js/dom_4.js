32435cc2e23a76f474b2ada652e2ab715d9f1579
jiwon22lee <jiwon1799@gmail.com>
2022-08-31 09:36:15 +0900

    전체동의

--- src/main/webapp/resources/js/dom_4.js
+++ src/main/webapp/resources/js/dom_4.js
@@ -14,12 +14,32 @@ const all = document.getElementById("all");
         
         if(all.checked==true){
         for(let i=0;i<cb.length;i++){
-            cb[i].checked=true;
+            cb[i].checked=all.checked;
         }
-    }else {
-        for(let y=0;y<cb.length;y++){
-            cb[y].checked=false;
         }
-    }
 }    
     )
+
+    
+
+    //----------------------------------------
+    for(let i =0;i<cb.length;i++){
+        // cb[i].addEventListener("click", function(){
+        //     check(1)
+        // });//매개변수를 이름있는 함수에 던질 수 없으니 임시함수 안에서 실행하여 매개변수던져야함.
+
+        cb[i].addEventListener("click", check);
+
+
+    }
+    
+    function check(){
+
+        for(let i =0;i<cb.length;i++){
+            if (!cb[i].checked){
+                result =false;
+                break;
+            }
+        }
+        all.checked=result;
+    }
